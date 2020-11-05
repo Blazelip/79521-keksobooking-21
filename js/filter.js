451cb1625f503ab91a41e8cfa6601d33e7d35ed3
Blazelip <filin1994@rambler.ru>
2020-11-05 12:05:26 +0300

    Мини правки, удалил коммент

--- js/filter.js
+++ js/filter.js
@@ -3,23 +3,6 @@
 (() => {
   const MAX_PIN_AMOUNT = 5;
   const FILTER_ALL = `any`;
-  // const filtersBlock = document.querySelector(`.map__filters`);
-  // const houseType = filtersBlock.querySelector(`#housing-type`);
-
-  // const onHouseSelectChange = () => {
-  //   const value = houseType.value;
-
-  //   const filteredData = window.dataWithId.filter((item) => {
-  //     return item.offer.type === value;
-  //   });
-
-  //   window.card.closeCurrentCard();
-  //   window.pin.deletePins();
-  //   window.pin.renderPins(filteredData);
-  // };
-
-  // houseType.addEventListener(`change`, onHouseSelectChange);
-
 
   const filtersForm = document.querySelector(`.map__filters`);
   const houseType = filtersForm.querySelector(`#housing-type`);
